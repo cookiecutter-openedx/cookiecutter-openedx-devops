83ea8de87d460b9925efcf4723eb56c54d9e860e
lpm0073 <lpm0073@gmail.com>
2022-03-15 16:00:58 -0600

    add data aws_caller_identity

--- {{cookiecutter.github_repo_name}}/terraform/components/eks_fargate/kubernetes.tf
+++ {{cookiecutter.github_repo_name}}/terraform/components/eks_fargate/kubernetes.tf
@@ -15,6 +15,8 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
+data "aws_caller_identity" "current" {}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
