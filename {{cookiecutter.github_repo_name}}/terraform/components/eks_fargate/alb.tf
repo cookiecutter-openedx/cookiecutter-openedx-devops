43f0c4e0fc32ac3962e18e84336c0e43082a3621
lpm0073 <lpm0073@gmail.com>
2022-03-15 21:09:06 -0600

    configure kubernetes_ingress. replace iam_policy eks_alb_ingress_controller w AWS json

--- {{cookiecutter.github_repo_name}}/terraform/components/eks_fargate/alb.tf
+++ {{cookiecutter.github_repo_name}}/terraform/components/eks_fargate/alb.tf
@@ -8,42 +8,8 @@
 #
 # see:
 # - https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.2/guide/ingress/annotations/
+# - https://aws.amazon.com/premiumsupport/knowledge-center/eks-alb-ingress-controller-fargate/
 #------------------------------------------------------------------------------ 
-
-resource "kubernetes_ingress" "app" {
-  metadata {
-    name      = "owncloud-lb"
-    namespace = "fargate-node"
-    annotations = {
-      "kubernetes.io/ingress.class"           = "alb"
-      "alb.ingress.kubernetes.io/scheme"      = "internet-facing"
-      "alb.ingress.kubernetes.io/target-type" = "ip"
-    }
-    labels = {
-        "app" = "owncloud"
-    }
-  }
-
-  spec {
-      backend {
-        service_name = "owncloud-service"
-        service_port = 80
-      }
-    rule {
-      http {
-        path {
-          path = "/"
-          backend {
-            service_name = "owncloud-service"
-            service_port = 80
-          }
-        }
-      }
-    }
-  }
-
-  depends_on = [kubernetes_service.app]
-}
 
 
 ################################################################################
@@ -58,25 +24,7 @@ resource "aws_iam_role" "eks_alb_ingress_controller" {
   name        = "eks-alb-ingress-controller"
   description = "Permissions required by the Kubernetes AWS ALB Ingress controller to do its job."
   force_detach_policies = true
-  assume_role_policy = <<ROLE
-      {
-        "Version": "2012-10-17",
-        "Statement": [
-          {
-            "Effect": "Allow",
-            "Principal": {
-              "Federated": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}"
-            },
-            "Action": "sts:AssumeRoleWithWebIdentity",
-            "Condition": {
-              "StringEquals": {
-                "${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub": "system:serviceaccount:kube-system:alb-ingress-controller"
-              }
-            }
-          }
-        ]
-      }
-      ROLE
+  assume_role_policy = file(".iam/iam_policy_alb_controller.json")
 }
 
 resource "aws_iam_role_policy_attachment" "ALB-policy_attachment" {
@@ -141,5 +89,61 @@ resource "kubernetes_service_account" "ingress" {
       "eks.amazonaws.com/role-arn" = aws_iam_role.eks_alb_ingress_controller.arn
     }
   }
+}
+
+################################################################################
+# Ingress for Application Load Balancer 
+################################################################################
+
+resource "kubernetes_ingress" "app" {
+  metadata {
+    name      = "owncloud-lb"
+    namespace = var.fargate_namespace
+    annotations = {
+      "kubernetes.io/ingress.class"                   = "alb"
+      "alb.ingress.kubernetes.io/scheme"              = "internet-facing"
+      "alb.ingress.kubernetes.io/target-type"         = "ip"
+      "alb.ingress.kubernetes.io/ip-address-type"     = "ipv4"
+
+      "alb.ingress.kubernetes.io/load-balancer-name"  = "${var.environment_namespace}"
+      "alb.ingress.kubernetes.io/group.name"          = "${var.environment_namespace}"
+      "alb.ingress.kubernetes.io/subnets"             = "${var.subnet_ids}"
+      "alb.ingress.kubernetes.io/security-groups"     = "SET ME PLEEASE"
+      "alb.ingress.kubernetes.io/load-balancer-attributes" = "stringMap"
+      "alb.ingress.kubernetes.io/listen-ports"        = jsonencode([{"HTTP": 80}, {"HTTPS": 443}, {"HTTP": 8080}, {"HTTPS": 8443}])
+      "alb.ingress.kubernetes.io/ssl-redirect"        = "443"
+      "alb.ingress.kubernetes.io/certificate-arn"     = "SET ME PLEASE"
+      "alb.ingress.kubernetes.io/backend-protocol"    = "HTTP"
+      "alb.ingress.kubernetes.io/success-codes"       = "'200' | '301'"
+      "alb.ingress.kubernetes.io/auth-session-cookie" = "AWSELBAuthSessionCookie"
+      "alb.ingress.kubernetes.io/auth-session-timeout" = "604800"
+
+    }
+    labels = {
+        "app" = "owncloud"
+    }
+  }
+
+  spec {
+      backend {
+        service_name = "owncloud-service"
+        service_port = 80
+      }
+    rule {
+      http {
+        path {
+          path = "/*"
+          backend {
+            service_name = kubernetes_service.app.metadata.0.name
+            service_port = 80
+          }
+        }
+      }
+    }
+  }
+  
+  depends_on = [kubernetes_service.app]
+
+  tags = var.tags
 }
 
