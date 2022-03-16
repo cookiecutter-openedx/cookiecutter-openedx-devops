d9923eaaa40efdb0a39f18ce7dbceb29e5bfdbcf
lpm0073 <lpm0073@gmail.com>
2022-03-16 16:22:14 -0600

    scaffold kubernetes, route53, acm

---
+++ {{cookiecutter.github_repo_name}}/terraform/components/eks_alb_ec2/kubernetes.tf
@@ -0,0 +1,24 @@
+#------------------------------------------------------------------------------
+# written by: Lawrence McDaniel
+#             https://lawrencemcdaniel.com/
+#
+# date: Mar-2022
+#
+# usage:  kubernetes configuration
+#------------------------------------------------------------------------------
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_caller_identity" "current" {}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
---
+++ {{cookiecutter.github_repo_name}}/terraform/components/eks_alb_ec2/versions.tf
@@ -0,0 +1,16 @@
+#------------------------------------------------------------------------------
+# written by: Lawrence McDaniel
+#             https://lawrencemcdaniel.com/
+#
+# date: Mar-2022
+#
+# usage: build an EKS cluster load balancer
+#------------------------------------------------------------------------------
+terraform {
+  required_version = "{{ cookiecutter.terraform_required_version }}"
+
+  required_providers {
+    aws        = "{{ cookiecutter.terraform_provider_hashicorp_aws_version }}"
+    kubernetes = "{{ cookiecutter.terraform_provider_kubernetes_version }}"
+  }
+}
