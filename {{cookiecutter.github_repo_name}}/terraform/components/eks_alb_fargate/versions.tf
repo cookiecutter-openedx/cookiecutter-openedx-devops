a000197999bab13290dab127164ec468023c33a6
lpm0073 <lpm0073@gmail.com>
2022-03-16 11:34:53 -0600

    lint

--- {{cookiecutter.github_repo_name}}/terraform/components/eks_alb_fargate/versions.tf
+++ {{cookiecutter.github_repo_name}}/terraform/components/eks_alb_fargate/versions.tf
@@ -7,13 +7,10 @@
 # usage: build an EKS cluster load balancer
 #------------------------------------------------------------------------------
 terraform {
-  # tflint-ignore
   required_version = "{{ cookiecutter.terraform_required_version }}"
 
   required_providers {
-    # tflint-ignore
-    aws = "{{ cookiecutter.terraform_provider_hashicorp_aws_version }}"
-    # tflint-ignore
+    aws        = "{{ cookiecutter.terraform_provider_hashicorp_aws_version }}"
     kubernetes = "{{ cookiecutter.terraform_provider_kubernetes_version }}"
   }
 }
