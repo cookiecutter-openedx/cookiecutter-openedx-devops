8e4713f850a3a5190f37526cbbb8265c22e1fd57
lpm0073 <lpm0073@gmail.com>
2022-03-17 09:59:35 -0600

    remove vpc_endpoints

--- {{cookiecutter.github_repo_name}}/terraform/components/vpc/outputs.tf
+++ {{cookiecutter.github_repo_name}}/terraform/components/vpc/outputs.tf
@@ -544,7 +544,7 @@ output "vpc_flow_log_cloudwatch_iam_role_arn" {
 }
 
 # VPC endpoints
-output "vpc_endpoints" {
-  description = "Array containing the full resource object and attributes for all endpoints created"
-  value       = module.vpc_endpoints.endpoints
-}
+#output "vpc_endpoints" {
+#  description = "Array containing the full resource object and attributes for all endpoints created"
+#  value       = module.vpc_endpoints.endpoints
+#}
