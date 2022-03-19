21e79012c30a3d7abe8f86d89ab27b68198ebcbf
lpm0073 <lpm0073@gmail.com>
2022-03-19 16:00:18 -0600

    migrated from terraform/components/kubernetes

--- {{cookiecutter.github_repo_name}}/terraform/components/eks_alb_ec2/eks_3_kubernetes.tf
+++ {{cookiecutter.github_repo_name}}/terraform/components/eks_alb_ec2/eks_3_kubernetes.tf
@@ -15,19 +15,14 @@ data "aws_eks_cluster_auth" "eks" {
   name = var.environment_namespace
 }
 
-data "aws_caller_identity" "current" {}
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
 }
 
-resource "kubernetes_namespace" "ec2" {
+resource "kubernetes_namespace" "namespace" {
   metadata {
-    labels = {
-      app = "my-app"
-    }
-    name = "ec2-node"
+    name = var.environment_namespace
   }
 }
