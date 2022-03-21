c4693e83b3083c5b8476bd743335b1db09d73011
lpm0073 <lpm0073@gmail.com>
2022-03-21 16:01:10 -0600

    point to alb

--- {{cookiecutter.github_repo_name}}/terraform/components/eks_alb/route53.tf
+++ {{cookiecutter.github_repo_name}}/terraform/components/eks_alb/route53.tf
@@ -14,6 +14,16 @@ data "aws_route53_zone" "environment_domain" {
   name = var.environment_domain
 }
 
+data "kubernetes_service" "ingress_alb_controller" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = var.k8s_namespace
+  }
+  depends_on = [module.alb_controller]
+}
+
+data "aws_elb_hosted_zone_id" "main" {}
+
 
 resource "aws_route53_record" "naked" {
   zone_id = data.aws_route53_zone.environment_domain.id
@@ -21,7 +31,7 @@ resource "aws_route53_record" "naked" {
   type    = "A"
 
   alias {
-    name                   = data.kubernetes_service.ingress_nginx_controller.load_balancer_ingress.0.hostname
+    name                   = data.kubernetes_service.ingress_alb_controller.load_balancer_ingress.0.hostname
     zone_id                = data.aws_elb_hosted_zone_id.main.id
     evaluate_target_health = true
   }
@@ -33,7 +43,7 @@ resource "aws_route53_record" "wildcard" {
   type    = "A"
 
   alias {
-    name                   = data.kubernetes_service.ingress_nginx_controller.load_balancer_ingress.0.hostname
+    name                   = data.kubernetes_service.ingress_alb_controller.load_balancer_ingress.0.hostname
     zone_id                = data.aws_elb_hosted_zone_id.main.id
     evaluate_target_health = true
   }
