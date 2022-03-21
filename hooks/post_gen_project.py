11ba346d23a81ad5b5d19068e856b9e67c911148
lpm0073 <lpm0073@gmail.com>
2022-03-21 12:58:45 -0600

    collapse eks_alb folders

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -12,42 +12,29 @@ HINT = "\x1b[3;33m"
 SUCCESS = "\x1b[1;32m [SUCCESS]: "
 
 
-def remove_eks_clb_ec2_files():
-    component_dir_path = os.path.join("terraform", "components", "eks_clb_ec2")
+def remove_eks_clb_files():
+    component_dir_path = os.path.join("terraform", "components", "eks_clb")
     if os.path.exists(component_dir_path):
         shutil.rmtree(component_dir_path)
 
-    terragrunt_dir_path = os.path.join("terraform", "environments", "{{ cookiecutter.environment_name }}", "eks_clb_ec2")
+    terragrunt_dir_path = os.path.join("terraform", "environments", "{{ cookiecutter.environment_name }}", "eks_clb")
     if os.path.exists(terragrunt_dir_path):
         shutil.rmtree(terragrunt_dir_path)
 
-    ci_dir_path = os.path.join("ci", "tutor-deploy", "environments", "{{ cookiecutter.environment_name }}", "k8s", "eks_clb_ec2")
+    ci_dir_path = os.path.join("ci", "tutor-deploy", "environments", "{{ cookiecutter.environment_name }}", "k8s", "eks_clb")
     if os.path.exists(ci_dir_path):
         shutil.rmtree(ci_dir_path)
 
-def remove_eks_alb_ec2_files():
-    component_dir_path = os.path.join("terraform", "components", "eks_alb_ec2")
+def remove_eks_alb_files():
+    component_dir_path = os.path.join("terraform", "components", "eks_alb")
     if os.path.exists(component_dir_path):
         shutil.rmtree(component_dir_path)
 
-    terragrunt_dir_path = os.path.join("terraform", "environments", "{{ cookiecutter.environment_name }}", "eks_alb_ec2")
+    terragrunt_dir_path = os.path.join("terraform", "environments", "{{ cookiecutter.environment_name }}", "eks_alb")
     if os.path.exists(terragrunt_dir_path):
         shutil.rmtree(terragrunt_dir_path)
 
-    ci_dir_path = os.path.join("ci", "tutor-deploy", "environments", "{{ cookiecutter.environment_name }}", "k8s", "eks_alb_ec2")
-    if os.path.exists(ci_dir_path):
-        shutil.rmtree(ci_dir_path)
-
-def remove_eks_abl_fargate_files():
-    component_dir_path = os.path.join("terraform", "components", "eks_alb_fargate")
-    if os.path.exists(component_dir_path):
-        shutil.rmtree(component_dir_path)
-
-    terragrunt_dir_path = os.path.join("terraform", "environments", "{{ cookiecutter.environment_name }}", "eks_alb_fargate")
-    if os.path.exists(terragrunt_dir_path):
-        shutil.rmtree(terragrunt_dir_path)
-
-    ci_dir_path = os.path.join("ci", "tutor-deploy", "environments", "{{ cookiecutter.environment_name }}", "k8s", "eks_alb_fargate")
+    ci_dir_path = os.path.join("ci", "tutor-deploy", "environments", "{{ cookiecutter.environment_name }}", "k8s", "eks_alb")
     if os.path.exists(ci_dir_path):
         shutil.rmtree(ci_dir_path)
 
@@ -64,20 +51,13 @@ def move_manifests(folder = ""):
 
 def main():
 
-    if "{{ cookiecutter.eks_cluster_compute_type }}" == "CLB_EC2":
-        remove_eks_abl_fargate_files()
-        remove_eks_alb_ec2_files()
-        move_manifests("eks_clb_ec2")
+    if "{{ cookiecutter.eks_cluster_compute_type }}" == "CLB":
+        remove_eks_alb_files()
+        move_manifests("eks_clb")
 
-    if "{{ cookiecutter.eks_cluster_compute_type }}" == "ALB_EC2":
-        remove_eks_abl_fargate_files()
-        remove_eks_clb_ec2_files()
-        move_manifests("eks_alb_ec2")
-
-    if "{{ cookiecutter.eks_cluster_compute_type }}" == "ALB_Fargate":
-        remove_eks_clb_ec2_files()
-        remove_eks_alb_ec2_files()
-        move_manifests("eks_alb_fargate")
+    if "{{ cookiecutter.eks_cluster_compute_type }}" == "ALB":
+        remove_eks_clb_files()
+        move_manifests("eks_alb")
 
     print(SUCCESS + "Your Open edX devops repo has been initialized." + TERMINATOR)
 
