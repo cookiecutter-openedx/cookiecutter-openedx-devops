eb5eb29ab2a8a37b776b1c4622e101da71d856d9
lpm0073 <lpm0073@gmail.com>
2022-03-22 13:01:28 -0600

    configure node groups

--- {{cookiecutter.github_repo_name}}/terraform/modules/eks/main.tf
+++ {{cookiecutter.github_repo_name}}/terraform/modules/eks/main.tf
@@ -52,43 +52,14 @@ resource "kubernetes_namespace" "openedx" {
 
 
 #------------------------------------------------------------------------------
-# FIX NOTE. make a decision on whether to use this or not.
-#
-# cluster_addons = {
-#   vpc-cni = {
-#     resolve_conflicts        = "OVERWRITE"
-#     service_account_role_arn = module.vpc_cni_irsa.iam_role_arn
-#   }
-# }
-#
-#------------------------------------------------------------------------------
-module "vpc_cni_irsa" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-
-  role_name             = "vpc_cni"
-  attach_vpc_cni_policy = true
-  vpc_cni_enable_ipv4   = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-node"]
-    }
-  }
-
-  tags = var.tags
-}
-
-#------------------------------------------------------------------------------
 # Technical documentation:
 # - https://docs.aws.amazon.com/eks
 # - https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/
 #
 #------------------------------------------------------------------------------
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "{{ cookiecutter.terraform_aws_modules_eks }}"
-
+  source                          = "terraform-aws-modules/eks/aws"
+  version                         = "{{ cookiecutter.terraform_aws_modules_eks }}"
   cluster_name                    = local.name
   cluster_version                 = var.eks_cluster_version
   cluster_endpoint_private_access = true
@@ -96,38 +67,28 @@ module "eks" {
   vpc_id                          = var.vpc_id
   subnet_ids                      = var.private_subnet_ids
   tags                            = var.tags
-  manage_aws_auth                 = false
-
-  # Note: https://docs.aws.amazon.com/eks/latest/userguide/fargate-getting-started.html#fargate-gs-coredns
-  cluster_addons = {
-    coredns = {
-      resolve_conflicts = "OVERWRITE"
-      namespace         = "kube-system"
-    }
-  }
-
-  node_groups = {
-    example = {
-      desired_capacity = 1
-
-      instance_types = ["t3.medium"]
-      k8s_labels = {
-        Example    = "managed_node_groups"
-        GithubRepo = "terraform-aws-eks"
-        GithubOrg  = "terraform-aws-modules"
+  eks_managed_node_groups = {
+    default = {
+      min_size       = var.eks_worker_group_min_size
+      max_size       = var.eks_worker_group_max_size
+      desired_size   = var.eks_worker_group_desired_size
+      instance_types = [var.eks_worker_group_instance_type]
+      labels = {
+        Environment = var.environment_namespace
+        GithubRepo  = "terraform-aws-eks"
+        GithubOrg   = "terraform-aws-modules"
       }
-      additional_tags = {
-        ExtraTag = "example"
-      }
-      update_config = {
-        max_unavailable_percentage = 50 # or set `max_unavailable`
+      tags = var.tags
+      timeouts = {
+        create = "10m"
+        delete = "10m"
       }
     }
   }
 
   fargate_profiles = {
-    default = {
-      name = "default"
+    openedx = {
+      name = "openedx"
       selectors = [
         {
           namespace = "kube-system"
@@ -136,41 +97,16 @@ module "eks" {
           }
         },
         {
-          namespace = "default"
+          namespace = "openedx"
           labels = {
             WorkerType = "fargate"
           }
         }
       ]
-
-      tags = {
-        Owner = "default"
-      }
-
+      tags = var.tags
       timeouts = {
-        create = "20m"
-        delete = "20m"
-      }
-    }
-
-    secondary = {
-      name = "secondary"
-      selectors = [
-        {
-          namespace = "default"
-          labels = {
-            Environment = "test"
-            GithubRepo  = "terraform-aws-eks"
-            GithubOrg   = "terraform-aws-modules"
-          }
-        }
-      ]
-
-      # Using specific subnets instead of the ones configured in EKS (`subnets` and `fargate_subnets`)
-      subnets = [module.vpc.private_subnets[1]]
-
-      tags = {
-        Owner = "secondary"
+        create = "10m"
+        delete = "10m"
       }
     }
   }
