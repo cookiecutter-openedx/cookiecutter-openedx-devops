7317674643462c8611a61d025c91eb212e91468e
lpm0073 <lpm0073@gmail.com>
2022-03-24 13:16:46 -0600

    testing

--- {{cookiecutter.github_repo_name}}/terraform/modules/vpc/certificate_manager.tf
+++ {{cookiecutter.github_repo_name}}/terraform/modules/vpc/certificate_manager.tf
@@ -66,25 +66,6 @@ module "acm_environment_environment_region" {
   tags                = var.tags
 }
 
-module "acm_subdomains_environment_region" {
-  source  = "terraform-aws-modules/acm/aws"
-  version = "{{ cookiecutter.terraform_aws_modules_acm }}"
-
-  providers = {
-    aws = aws.environment_region
-  }
-
-  count       = length(var.subdomains)
-  domain_name = aws_route53_zone.subdomain[count.index].name
-  zone_id     = aws_route53_zone.subdomain[count.index].id
-
-  subject_alternative_names = [
-    "*.${aws_route53_zone.subdomain[count.index].name}",
-  ]
-
-  wait_for_validation = true
-  tags                = var.tags
-}
 
 #------------------------------------------------------------------------------
 # SSL/TLS certs issued in us-east-1 for Cloudfront
