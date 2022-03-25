2fd5f26c0353e57707003a2cd2ac16a8d6dc5300
lpm0073 <lpm0073@gmail.com>
2022-03-25 13:11:45 -0600

    configure aws alb controller based on https://aws.amazon.com/premiumsupport/knowledge-center/eks-alb-ingress-controller-fargate/

--- {{cookiecutter.github_repo_name}}/terraform/modules/eks/alb-controller.tf
+++ {{cookiecutter.github_repo_name}}/terraform/modules/eks/alb-controller.tf
@@ -22,13 +22,15 @@
 #------------------------------------------------------------------------------
 
 locals {
-  k8s_cluster_type          = "eks"
-  k8s_namespace             = "ingress-alb-controller"
-  k8s_replicas              = 2
-  alb_controller_depends_on = [data.aws_eks_cluster.cluster]
+  k8s_namespace = "ingress-alb-controller"
+  resource_name = "aws-load-balancer-controller"
+  alb_controller_depends_on = [
+    data.aws_eks_cluster.cluster,
+    kubernetes_namespace.ingress-alb-controller
+  ]
 }
 
-
+# mcdaniel: made by me
 resource "kubernetes_namespace" "ingress-alb-controller" {
   metadata {
     annotations = {
@@ -43,8 +45,13 @@ resource "kubernetes_namespace" "ingress-alb-controller" {
   }
 }
 
+#------------------------------------------------------------------------------
+# https://aws.amazon.com/premiumsupport/knowledge-center/eks-alb-ingress-controller-fargate/
+#
+# 2. To allow the cluster to use AWS Identity and Access Management (IAM) for service accounts, run the following command:
+#    eksctl utils associate-iam-oidc-provider --cluster var.environment_namespace --approve
+#------------------------------------------------------------------------------
 data "aws_caller_identity" "current" {}
-
 data "aws_iam_policy_document" "eks_oidc_assume_role" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -66,21 +73,31 @@ data "aws_iam_policy_document" "eks_oidc_assume_role" {
 }
 
 resource "aws_iam_role" "this" {
-  name        = substr("${var.aws_resource_name_prefix}${var.environment_namespace}-aws-load-balancer-controller", 0, 64)
-  description = "Permissions required by the Kubernetes AWS Load Balancer controller to do its job."
-
-  tags = var.tags
-
+  name                  = "${var.environment_namespace}-aws-load-balancer-controller"
+  description           = "Permissions required by the Kubernetes AWS Load Balancer controller to do its job."
+  tags                  = var.tags
   force_detach_policies = true
-
-  assume_role_policy = data.aws_iam_policy_document.eks_oidc_assume_role.json
+  assume_role_policy    = data.aws_iam_policy_document.eks_oidc_assume_role.json
 }
 
+#------------------------------------------------------------------------------
+# https://aws.amazon.com/premiumsupport/knowledge-center/eks-alb-ingress-controller-fargate/
+#
+# 3. To download an IAM policy that allows the AWS Load Balancer Controller to make calls to AWS APIs on your behalf, run the following command:
+#    curl -o ./json/iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.2.0/docs/install/iam_policy.json
+#
+# 4. To create an IAM policy using the policy that you downloaded in step 3, run the following command:
+#    aws iam create-policy \
+#       --policy-name AWSLoadBalancerControllerIAMPolicy \
+#       --policy-document file://iam_policy.json
+#
+# need to review these alternatives
+# https://docs.aws.amazon.com/elasticloadbalancing/latest/userguide/load-balancer-authentication-access-control.html
+#------------------------------------------------------------------------------
 resource "aws_iam_policy" "this" {
-  name        = "${var.environment_namespace}-alb-management"
-  description = "Permissions that are required to manage AWS Application Load Balancers."
-  # Source: `curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.3.1/docs/install/iam_policy.json`
-  policy = file("${path.module}/json/iam-policy.json")
+  name        = "AWSLoadBalancerControllerIAMPolicy"
+  description = "Allows the AWS Load Balancer Controller to make calls to AWS APIs on your behalf."
+  policy      = file("${path.module}/json/iam-policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "this" {
@@ -88,18 +105,30 @@ resource "aws_iam_role_policy_attachment" "this" {
   role       = aws_iam_role.this.name
 }
 
+#------------------------------------------------------------------------------
+# https://aws.amazon.com/premiumsupport/knowledge-center/eks-alb-ingress-controller-fargate/
+#
+# 5. To create a service account named aws-load-balancer-controller in the
+#    kube-system namespace for the AWS Load Balancer Controller,
+#    run the following command:
+#     eksctl create iamserviceaccount \
+#         --cluster=YOUR_CLUSTER_NAME \
+#         --namespace=kube-system \
+#         --name=aws-load-balancer-controller \
+#         --attach-policy-arn=arn:aws:iam::<AWS_ACCOUNT_ID>:policy/AWSLoadBalancerControllerIAMPolicy \
+#         --override-existing-serviceaccounts \
+#         --approve
+#------------------------------------------------------------------------------
 resource "kubernetes_service_account" "this" {
   automount_service_account_token = true
   metadata {
-    name      = "aws-load-balancer-controller"
-    namespace = var.k8s_namespace
+    name      = var.resource_name
+    namespace = var.k8s_namespace // note that example uses `kube-system`
     annotations = {
-      # This annotation is only used when running on EKS which can
-      # use IAM roles for service accounts.
       "eks.amazonaws.com/role-arn" = aws_iam_role.this.arn
     }
     labels = {
-      "app.kubernetes.io/name"       = "aws-load-balancer-controller"
+      "app.kubernetes.io/name"       = var.resource_name
       "app.kubernetes.io/component"  = "controller"
       "app.kubernetes.io/managed-by" = "terraform"
     }
@@ -107,12 +136,35 @@ resource "kubernetes_service_account" "this" {
   depends_on = [var.alb_controller_depends_on]
 }
 
-resource "kubernetes_cluster_role" "this" {
+resource "kubernetes_cluster_role_binding" "this" {
   metadata {
-    name = "aws-load-balancer-controller"
+    name = var.resource_name
 
     labels = {
-      "app.kubernetes.io/name"       = "aws-load-balancer-controller"
+      "app.kubernetes.io/name"       = var.resource_name
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.this.metadata[0].name
+  }
+
+  subject {
+    api_group = ""
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.this.metadata[0].name
+    namespace = kubernetes_service_account.this.metadata[0].namespace
+  }
+}
+resource "kubernetes_cluster_role" "this" {
+  metadata {
+    name = var.resource_name
+
+    labels = {
+      "app.kubernetes.io/name"       = var.resource_name
       "app.kubernetes.io/managed-by" = "terraform"
     }
   }
@@ -165,136 +217,47 @@ resource "kubernetes_cluster_role" "this" {
   depends_on = [var.alb_controller_depends_on]
 }
 
-resource "kubernetes_cluster_role_binding" "this" {
-  metadata {
-    name = "aws-load-balancer-controller"
-
-    labels = {
-      "app.kubernetes.io/name"       = "aws-load-balancer-controller"
-      "app.kubernetes.io/managed-by" = "terraform"
-    }
-  }
-
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = kubernetes_cluster_role.this.metadata[0].name
-  }
-
-  subject {
-    api_group = ""
-    kind      = "ServiceAccount"
-    name      = kubernetes_service_account.this.metadata[0].name
-    namespace = kubernetes_service_account.this.metadata[0].namespace
-  }
+# mcdaniel: i made this. probably redundant.
+resource "aws_iam_role_policy_attachment" "cluster_role" {
+  policy_arn = aws_iam_policy.this.arn
+  role       = kubernetes_cluster_role.this.name
 }
 
-resource "helm_release" "alb_controller" {
+#------------------------------------------------------------------------------
+# https://aws.amazon.com/premiumsupport/knowledge-center/eks-alb-ingress-controller-fargate/
+#
+# 6. To verify that the new service role was created, run the following command:
+#    eksctl get iamserviceaccount --cluster YOUR_CLUSTER_NAME --name aws-load-balancer-controller --namespace kube-system
+#
+#    or
+#
+#    kubectl get serviceaccount aws-load-balancer-controller --namespace ingress-alb-controller
+#------------------------------------------------------------------------------
 
-  name       = "aws-load-balancer-controller"
+
+#------------------------------------------------------------------------------
+# https://aws.amazon.com/premiumsupport/knowledge-center/eks-alb-ingress-controller-fargate/
+#
+# Install the AWS Load Balancer Controller using Helm:
+#
+#    helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
+#          --set clusterName=YOUR_CLUSTER_NAME \
+#          --set serviceAccount.create=false \
+#          --set region=YOUR_REGION_CODE \
+#          --set vpcId=<VPC_ID> \
+#          --set serviceAccount.name=aws-load-balancer-controller \
+#          -n kube-system
+#
+#------------------------------------------------------------------------------
+resource "helm_release" "alb_controller" {
+  name       = var.resource_name
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
   version    = "{{ cookiecutter.terraform_helm_alb_controller_chart_version }}"
-  namespace  = var.k8s_namespace
-  atomic     = true
-  timeout    = 900
 
-  dynamic "set" {
-
-    for_each = {
-      "clusterName"           = var.environment_namespace
-      "serviceAccount.create" = false
-      "serviceAccount.name"   = kubernetes_service_account.this.metadata[0].name
-      "region"                = var.aws_region
-      "vpcId"                 = var.vpc_id
-      "hostNetwork"           = false
-    }
-    content {
-      name  = set.key
-      value = set.value
-    }
-  }
-
-  dynamic "set" {
-    for_each = var.chart_env_overrides
-    content {
-      name  = set.key
-      value = set.value
-    }
-  }
+  namespace = var.k8s_namespace
+  atomic    = true
+  timeout   = 900
 
   depends_on = [var.alb_controller_depends_on]
-}
-
-# Generate a kubeconfig file for the EKS cluster to use in provisioners
-data "template_file" "kubeconfig" {
-  template = <<-EOF
-    apiVersion: v1
-    kind: Config
-    current-context: terraform
-    clusters:
-    - name: ${data.aws_eks_cluster.cluster.name}
-      cluster:
-        certificate-authority-data: ${data.aws_eks_cluster.cluster.certificate_authority.0.data}
-        server: ${data.aws_eks_cluster.cluster.endpoint}
-    contexts:
-    - name: terraform
-      context:
-        cluster: ${data.aws_eks_cluster.cluster.name}
-        user: terraform
-    users:
-    - name: terraform
-      user:
-        token: ${data.aws_eks_cluster_auth.cluster.token}
-  EOF
-}
-
-# Since the kubernetes_provider cannot yet handle CRDs, we need to set any
-# supplied TargetGroupBinding using a null_resource.
-#
-# The method used below for securely specifying the kubeconfig to provisioners
-# without spilling secrets into the logs comes from:
-# https://medium.com/citihub/a-more-secure-way-to-call-kubectl-from-terraform-1052adf37af8
-#
-# The method used below for referencing external resources in a destroy
-# provisioner via triggers comes from
-# https://github.com/hashicorp/terraform/issues/23679#issuecomment-886020367
-resource "null_resource" "supply_target_group_arns" {
-  count = (length(var.target_groups) > 0) ? length(var.target_groups) : 0
-
-  triggers = {
-    kubeconfig  = base64encode(data.template_file.kubeconfig.rendered)
-    cmd_create  = <<-EOF
-      cat <<YAML | kubectl -n ${var.k8s_namespace} --kubeconfig <(echo $KUBECONFIG | base64 --decode) apply -f -
-      apiVersion: elbv2.k8s.aws/v1beta1
-      kind: TargetGroupBinding
-      metadata:
-        name: ${lookup(var.target_groups[count.index], "name", "")}-tgb
-      spec:
-        serviceRef:
-          name: ${lookup(var.target_groups[count.index], "name", "")}
-          port: ${lookup(var.target_groups[count.index], "backend_port", "")}
-        targetGroupARN: ${lookup(var.target_groups[count.index], "target_group_arn", "")}
-        targetType:  ${lookup(var.target_groups[count.index], "target_type", "instance")}
-      YAML
-    EOF
-    cmd_destroy = "kubectl -n ${var.k8s_namespace} --kubeconfig <(echo $KUBECONFIG | base64 --decode) delete TargetGroupBinding ${lookup(var.target_groups[count.index], "name", "")}-tgb"
-  }
-
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    environment = {
-      KUBECONFIG = self.triggers.kubeconfig
-    }
-    command = self.triggers.cmd_create
-  }
-  provisioner "local-exec" {
-    when        = destroy
-    interpreter = ["/bin/bash", "-c"]
-    environment = {
-      KUBECONFIG = self.triggers.kubeconfig
-    }
-    command = self.triggers.cmd_destroy
-  }
-  depends_on = [helm_release.alb_controller]
 }
---
+++ {{cookiecutter.github_repo_name}}/terraform/modules/eks/json/README.md
@@ -0,0 +1,4 @@
+
+## Source
+
+`curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.2.0/docs/install/iam_policy.json`
