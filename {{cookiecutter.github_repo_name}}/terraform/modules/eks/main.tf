dadd6b7a597775200cff5fb696ecd062bfd940be
lpm0073 <lpm0073@gmail.com>
2022-03-27 07:51:47 -0600

    move coredns to fargate

--- {{cookiecutter.github_repo_name}}/terraform/modules/eks/main.tf
+++ {{cookiecutter.github_repo_name}}/terraform/modules/eks/main.tf
@@ -24,6 +24,17 @@ module "eks" {
   subnet_ids                      = var.private_subnet_ids
   tags                            = var.tags
 
+  # Note: https://docs.aws.amazon.com/eks/latest/userguide/fargate-getting-started.html#fargate-gs-coredns
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    #kube-proxy = {}
+    #vpc-cni = {
+    #  resolve_conflicts = "OVERWRITE"
+    #}
+  }
+
   # FIX NOTE:
   # regarding https://github.com/kubernetes-sigs/aws-load-balancer-controller/issues/2462
   # this is allowing all traffic -- the group is wide open.
@@ -32,6 +43,8 @@ module "eks" {
   # Error: Failed to create Ingress 'ingress-alb-controller/nginx-lb' because: Internal error occurred:
   #        failed calling webhook "vingress.elbv2.k8s.aws":
   #        Post "https://aws-load-balancer-webhook-service.ingress-alb-controller.svc:443/validate-networking-v1-ingress?timeout=10s": context deadline exceeded
+  #
+  # private subnet cidr is 192.168.0.0/16 ?
   node_security_group_additional_rules = {
     inter_node = {
       description = "Allow traffic between nodes and control plane"
@@ -44,26 +57,44 @@ module "eks" {
   }
 
 
-  eks_managed_node_groups = {
-    default = {
-      min_size       = var.eks_worker_group_min_size
-      max_size       = var.eks_worker_group_max_size
-      desired_size   = var.eks_worker_group_desired_size
-      instance_types = [var.eks_worker_group_instance_type]
-      labels = {
-        Environment = var.environment_namespace
-        GithubRepo  = "terraform-aws-eks"
-        GithubOrg   = "terraform-aws-modules"
-      }
-      tags = var.tags
-      timeouts = {
-        create = "10m"
-        delete = "10m"
-      }
-    }
-  }
+  #eks_managed_node_groups = {
+  #  default = {
+  #    min_size       = var.eks_worker_group_min_size
+  #    max_size       = var.eks_worker_group_max_size
+  #    desired_size   = var.eks_worker_group_desired_size
+  #    instance_types = [var.eks_worker_group_instance_type]
+  #    labels = {
+  #      Environment = var.environment_namespace
+  #      GithubRepo  = "terraform-aws-eks"
+  #      GithubOrg   = "terraform-aws-modules"
+  #    }
+  #    tags = var.tags
+  #    timeouts = {
+  #      create = "10m"
+  #      delete = "10m"
+  #    }
+  #  }
+  #}
 
-  fargate_profiles = {
+  coredns = {
+    coredns = {
+      name       = "coredns"
+      subnet_ids = var.private_subnet_ids
+
+      selectors = [
+        {
+          namespace = "kube-system"
+          labels = {
+            k8s-app = "kube-dns"
+          }
+        }
+      ]
+      tags = var.tags
+      # this is redundant, since aws_iam_role.this sets its assume_role_policy
+      # to point to this exact fargate profile.
+      pod_execution_role = aws_iam_role.pod_execution_role
+    }
+
     fargate-node = {
       name = "fargate-node"
       selectors = [
