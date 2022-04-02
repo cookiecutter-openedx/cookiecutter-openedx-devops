633ad8af9f4bf4dc2b5424c450ebb031ac9e73d5
lpm0073 <lpm0073@gmail.com>
2022-04-02 16:59:26 -0600

    arrrrrrrrrrrrrr

--- {{cookiecutter.github_repo_name}}/terraform/modules/kubernetes_ingress_clb/ingress.tf
+++ {{cookiecutter.github_repo_name}}/terraform/modules/kubernetes_ingress_clb/ingress.tf
@@ -50,7 +50,7 @@ resource "aws_route53_record" "ingress_domains_wildcard" {
   type    = "A"
 
   alias {
-    name                   = data.kubernetes_service.ingress_nginx_controller.load_balancer_ingress.0.hostname
+    name                   = data.kubernetes_service.ingress_nginx_controller.status.0.load_balancer.0.ingress.0.hostname
     zone_id                = data.aws_elb_hosted_zone_id.main.id
     evaluate_target_health = true
   }
@@ -63,7 +63,7 @@ resource "aws_route53_record" "ingress_domains_naked" {
   type    = "A"
 
   alias {
-    name                   = data.kubernetes_service.ingress_nginx_controller.load_balancer_ingress.0.hostname
+    name                   = data.kubernetes_service.ingress_nginx_controller.status.0.load_balancer.0.ingress.0.hostname
     zone_id                = data.aws_elb_hosted_zone_id.main.id
     evaluate_target_health = true
   }
