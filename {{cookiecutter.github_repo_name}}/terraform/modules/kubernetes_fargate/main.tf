fd912bf573a4e6cb6a9316042d356bd24c743f95
lpm0073 <lpm0073@gmail.com>
2022-04-01 18:35:44 -0600

    move port 80 sg to alb controller

--- {{cookiecutter.github_repo_name}}/terraform/modules/kubernetes_fargate/main.tf
+++ {{cookiecutter.github_repo_name}}/terraform/modules/kubernetes_fargate/main.tf
@@ -118,6 +118,25 @@ module "eks" {
       ipv6_cidr_blocks = ["::/0"]
     }
   }
+  cluster_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+    egress_all = {
+      description      = "Node all egress"
+      protocol         = "-1"
+      from_port        = 0
+      to_port          = 0
+      type             = "egress"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  }
 
   eks_managed_node_groups = {
     default = {
@@ -151,62 +170,6 @@ module "eks" {
 
 }
 
-#------------------------------------------------------------------------------
-# This security group is created automatically by the EKS and is one of three
-# security groups associated with the cluster. The terraform module
-# terraform-aws-modules/eks/aws, above, provides hooks for the other two, but
-# does not provide us with a way to modify this one.
-#
-# We have to rely on the kubernetes-managed resource tags to identify it.
-#
-# Also, note that this security group is identifiable in the AWS Console
-# with the following description: "EKS created security group applied to ENI that
-#     is attached to EKS Control Plane master nodes, as well as any managed
-#     workloads."
-#------------------------------------------------------------------------------
-data "aws_security_group" "eks" {
-  tags = merge(
-    {
-      "kubernetes.io/cluster/${var.environment_namespace}" = "owned"
-    },
-    {
-      "aws:eks:cluster-name" = "${var.environment_namespace}"
-    },
-  )
-
-  depends_on = [module.eks]
-}
-
-# we need this so that we can pass the cidr of the vpc
-# to the security group rule below.
-data "aws_vpc" "environment" {
-
-  filter {
-    name   = "tag-value"
-    values = ["${var.environment_namespace}"]
-  }
-  filter {
-    name   = "tag-key"
-    values = ["Name"]
-  }
-
-}
-
-#------------------------------------------------------------------------------
-# mcdaniel mar-2022
-# this is needed so that Fargate nodes can receive traffic from resources
-# inside the VPC; namely, the ALB.
-#------------------------------------------------------------------------------
-resource "aws_security_group_rule" "nginx" {
-  description       = "http port 80 from inside the VPC"
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = [data.aws_vpc.environment.cidr_block]
-  security_group_id = data.aws_security_group.eks.id
-  depends_on        = [module.eks]
-}
 
 #------------------------------------------------------------------------------
 # mcdaniel mar-2022
---
+++ {{cookiecutter.github_repo_name}}/terraform/modules/kubernetes_ingress_alb_controller/security_groups.tf
@@ -0,0 +1,56 @@
+#------------------------------------------------------------------------------
+# This security group is created automatically by the EKS and is one of three
+# security groups associated with the cluster. The terraform module
+# terraform-aws-modules/eks/aws, above, provides hooks for the other two, but
+# does not provide us with a way to modify this one.
+#
+# We have to rely on the kubernetes-managed resource tags to identify it.
+#
+# Also, note that this security group is identifiable in the AWS Console
+# with the following description: "EKS created security group applied to ENI that
+#     is attached to EKS Control Plane master nodes, as well as any managed
+#     workloads."
+#------------------------------------------------------------------------------
+data "aws_security_group" "eks" {
+  tags = merge(
+    {
+      "kubernetes.io/cluster/${var.environment_namespace}" = "owned"
+    },
+    {
+      "aws:eks:cluster-name" = "${var.environment_namespace}"
+    },
+  )
+
+  depends_on = [module.eks]
+}
+
+# we need this so that we can pass the cidr of the vpc
+# to the security group rule below.
+data "aws_vpc" "environment" {
+
+  filter {
+    name   = "tag-value"
+    values = ["${var.environment_namespace}"]
+  }
+  filter {
+    name   = "tag-key"
+    values = ["Name"]
+  }
+
+}
+
+#------------------------------------------------------------------------------
+# mcdaniel mar-2022
+# this is needed so that Fargate nodes can receive traffic from resources
+# inside the VPC; namely, the ALB.
+#------------------------------------------------------------------------------
+resource "aws_security_group_rule" "nginx" {
+  description       = "http port 80 from inside the VPC"
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = [data.aws_vpc.environment.cidr_block]
+  security_group_id = data.aws_security_group.eks.id
+  depends_on        = [module.eks]
+}
