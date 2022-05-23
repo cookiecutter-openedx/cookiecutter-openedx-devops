f565d7fdd0d2560636142d7261fdf32f82b1c798
lpm0073 <lpm0073@gmail.com>
2022-05-23 13:07:56 -0500

    ingresses are environment specific

--- {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes_ingress_clb/outputs.tf
+++
@@ -1,8 +0,0 @@
-#------------------------------------------------------------------------------
-# written by: Miguel Afonso
-#             https://www.linkedin.com/in/mmafonso/
-#
-# date: Aug-2021
-#
-# usage: build an EKS cluster load balancer
-#------------------------------------------------------------------------------
