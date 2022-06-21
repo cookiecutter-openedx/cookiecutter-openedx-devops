bf65f163347716ce2cd411308bb17973f5d746be
lpm0073 <lpm0073@gmail.com>
2022-06-21 14:07:03 -0500

    add bastion setup script

---
+++ {{cookiecutter.github_repo_name}}/scripts/bastion-config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#---------------------------------------------------------
+# written by: Lawrence McDaniel
+#             https://lawrencemcdaniel.com
+#
+# date:       june-2022
+#
+# usage: run this on your EC2 Ubuntu bastion instance
+#        to install software packages that are required for use
+#        of k8s, kubectl, aws cli, and mysql
+#--------------------------------------------------------
+sudo apt update
+sudo apt upgrade -y
+sudo apt install awscli jq
+sudo snap install kubectl --channel=1.23/stable --classic
+sudo snap install yq
+pip install --upgrade pyyaml
+pip install tutor
+sudo apt autoremove
