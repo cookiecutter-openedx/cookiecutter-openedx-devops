b69d2c14affb8c784e1b46a85e458c523cc37227
lpm0073 <lpm0073@gmail.com>
2022-06-25 10:14:08 -0500

    add homebrew, terraform, terragrunt

--- {{cookiecutter.github_repo_name}}/scripts/bastion-config.sh
+++ {{cookiecutter.github_repo_name}}/scripts/bastion-config.sh
@@ -29,3 +29,18 @@ pip install tutor
 # install k9s
 sudo wget -qO- https://github.com/derailed/k9s/releases/download/v0.24.1/k9s_Linux_x86_64.tar.gz | tar zxvf -  -C /tmp/
 sudo mv /tmp/k9s /usr/local/bin
+
+# install homebrew
+# installation time: around 15 minutes
+# -------------------------------------------------------------
+# sudo passwd ubuntu
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/ubuntu/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential
+brew install gcc
+
+# install terraform tools
+# -------------------------------------------------------------
+brew install terraform
+brew install terragrunt
