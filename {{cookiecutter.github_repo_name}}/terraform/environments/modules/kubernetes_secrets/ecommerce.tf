fd83bb05a0471033ac4afccb3ccd75870705bcc2
lpm0073 <lpm0073@gmail.com>
2022-07-15 12:22:30 -0500

    add ecommerce config secret

---
+++ {{cookiecutter.github_repo_name}}/terraform/environments/modules/kubernetes_secrets/ecommerce.tf
@@ -0,0 +1,20 @@
+#------------------------------------------------------------------------------
+# written by = Lawrence McDaniel
+#             https://lawrencemcdaniel.com/
+#
+# date = July-2022
+#
+# usage = scaffold the yaml-formatted credentials data for ecommerce configuration.
+#        assuming that we'll later revisit this data from k9s, to add keys and secrets.
+#------------------------------------------------------------------------------
+
+resource "kubernetes_secret" "ecommerce_config" {
+  metadata {
+    name      = "ecommerce-config"
+    namespace = var.namespace
+  }
+
+  data = {
+    PAYMENT_PROCESSOR_CONFIG = file("${path.module}/ecommerce-config.yml")
+  }
+}
