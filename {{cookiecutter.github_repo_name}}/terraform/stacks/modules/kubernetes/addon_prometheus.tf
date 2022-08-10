65265b33bf2fedd619a13625afe2503e242f0ebc
lpm0073 <lpm0073@gmail.com>
2022-08-10 15:47:46 -0500

    add prometheus configuration options

--- {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes/addon_prometheus.tf
+++ {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes/addon_prometheus.tf
@@ -12,6 +12,11 @@
 #   helm repo update
 #   helm search repo prometheus-community
 #
+# Configuration: all configurations options are located in ./yml/kube-prometheus-config.yaml
+#   you can update the contents of this yaml file by running the following:
+#
+#   helm show values prometheus-community/kube-prometheus-stack
+#
 # NOTE: run `helm repo update` prior to running this
 #       Terraform module.
 #-----------------------------------------------------------
@@ -24,6 +29,11 @@ resource "helm_release" "prometheus" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
   version    = "{{ cookiecutter.terraform_helm_kube_prometheus }}"
+
+  values = [
+    "${file("${path.module}/yml/kube-prometheus-config.yaml")}"
+  ]
+
 
   depends_on = [
     module.eks,
