f967225bf7098622a727dccb12d57b54002bc854
lpm0073 <lpm0073@gmail.com>
2022-08-10 12:28:32 -0500

    add kube-prometheus-stack helm chart

--- {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes/addon_prometheus.tf
+++ {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes/addon_prometheus.tf
@@ -11,7 +11,22 @@
 #   brew install helm
 #   helm repo add prometheus-community https://prometheus-community.github.io/helm-charts/
 #   helm repo update
+#   helm search repo prometheus-community
 #
 # NOTE: run `helm repo update` prior to running this
 #       Terraform module.
 #-----------------------------------------------------------
+
+resource "helm_release" "prometheus" {
+  namespace        = "monitoring"
+  create_namespace = true
+
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "{{ cookiecutter.terraform_helm_kube_prometheus }}"
+
+  depends_on = [
+    module.eks,
+  ]
+}
