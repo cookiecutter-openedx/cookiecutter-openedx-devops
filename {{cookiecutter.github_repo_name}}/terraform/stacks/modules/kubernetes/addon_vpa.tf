3ed292bcbb58a074879686b842c1ace52a27a5f9
lpm0073 <lpm0073@gmail.com>
2022-08-15 09:17:05 -0500

    add k8s vertical pod autoscaler

---
+++ {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes/addon_vpa.tf
@@ -0,0 +1,35 @@
+#------------------------------------------------------------------------------
+# written by: Lawrence McDaniel
+#             https://lawrencemcdaniel.com/
+#
+# date: Aug-2022
+#
+# usage: installs the Kubernetes Vertical Pod Autoscaler.
+#
+# see:  https://www.datree.io/helm-chart/vertical-pod-autoscaler-helm
+#
+# requirements: you must initialize a local helm repo in order to run
+# this mdoule.
+#
+#   brew install helm
+#   helm repo add cowboysysop https://cowboysysop.github.io/charts/
+#   helm repo update
+#   helm search repo cowboysysop
+#
+#
+# NOTE: run `helm repo update` prior to running this
+#       Terraform module.
+#-----------------------------------------------------------
+resource "helm_release" "vpa" {
+  namespace        = "monitoring"
+  create_namespace = true
+
+  name       = "vertical-pod-autoscaler"
+  repository = "https://cowboysysop.github.io/charts/"
+  chart      = "vertical-pod-autoscaler"
+  version    = "5.1.1"
+
+  depends_on = [
+    module.eks,
+  ]
+}
