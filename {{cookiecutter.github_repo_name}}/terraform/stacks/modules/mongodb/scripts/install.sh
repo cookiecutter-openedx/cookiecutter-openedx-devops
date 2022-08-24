23c128da5fcc80787250a54c9eefa575911a33b9
lpm0073 <lpm0073@gmail.com>
2022-08-23 21:13:40 -0500

    mount the EBS volume

--- {{cookiecutter.github_repo_name}}/terraform/stacks/modules/mongodb/scripts/install.sh
+++ {{cookiecutter.github_repo_name}}/terraform/stacks/modules/mongodb/scripts/install.sh
@@ -3,11 +3,20 @@
 # written by: Lawrence McDaniel
 #             https://lawrencemcdaniel.com
 #
-# date:       june-2022
+# date:       aug-2022
 #
 # usage: run this on your MongoDB Ubuntu bastion instance
 #        to install required software packages
 #--------------------------------------------------------
+
+#
+# mount the detachable EBS volume to mongodb's data directory.
+if [ ! -d "/var/lib/mongodb" ]
+then
+sudo mkfs -t ext4 /dev/nvme1n1
+sudo mkdir /var/lib/mongodb -p
+sudo mount /dev/nvme1n1 /var/lib/mongodb
+fi
 
 #
 # Install Mongodb 4.2.x
@@ -23,6 +32,9 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 4B7C549A058F8B
 echo "deb [arch=amd64] http://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt update
 sudo apt install mongodb-org
+
+sudo chown -R mongodb /var/lib/mongodb
+sudo chgrp -R mongodb /var/lib/mongodb
 
 sudo systemctl enable mongod.service
 sudo systemctl start mongod.service
