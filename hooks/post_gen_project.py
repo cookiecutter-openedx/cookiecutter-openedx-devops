dd625b1a194739a87a66a451651e0ee5f68eeed0
lpm0073 <lpm0073@gmail.com>
2022-08-29 15:19:21 -0500

    process mongodb option

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,25 +11,43 @@ INFO = "\x1b[1;33m [INFO]: "
 HINT = "\x1b[3;33m"
 SUCCESS = "\x1b[1;32m [SUCCESS]: "
 
+def rm_directory(dir_path: str):
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
 
 def remove_bastion():
-    module_dir_path = os.path.join("terraform", "modules", "ec2_bastion")
-    if os.path.exists(module_dir_path):
-        shutil.rmtree(module_dir_path)
+    dir_path = os.path.join("terraform", "stacks", "modules", "ec2_bastion")
+    rm_directory(dir_path)
 
-    terragrunt_dir_path = os.path.join(
-        "terraform",
-        "environments",
-        "{{ cookiecutter.environment_name }}",
-        "ec2_bastion",
-    )
-    if os.path.exists(terragrunt_dir_path):
-        shutil.rmtree(terragrunt_dir_path)
+    dir_path = os.path.join("terraform", "stacks", "{{ cookiecutter.environment_name }}", "ec2_bastion")
+    rm_directory(dir_path)
 
+    dir_path = os.path.join("terraform", "environments", "modules", "ec2_bastion")
+    rm_directory(dir_path)
+
+    dir_path = os.path.join("terraform", "environments", "{{ cookiecutter.environment_name }}", "ec2_bastion")
+    rm_directory(dir_path)
+
+
+def remove_mongodb():
+    dir_path = os.path.join("terraform", "stacks", "modules", "mongodb")
+    rm_directory(dir_path)
+
+    dir_path = os.path.join("terraform", "stacks", "{{ cookiecutter.environment_name }}", "mongodb")
+    rm_directory(dir_path)
+
+    dir_path = os.path.join("terraform", "environments", "{{ cookiecutter.environment_name }}", "mongodb")
+    rm_directory(dir_path)
+
+    dir_path = os.path.join("terraform", "environments", "modules", "mongodb")
+    rm_directory(dir_path)
 
 def main():
-    if "{{ cookiecutter.environment_add_bastion }}".upper() != "Y":
+    if "{{ cookiecutter.stack_add_bastion }}".upper() != "Y":
         remove_bastion()
+
+    if "{{ cookiecutter.stack_add_remote_mongodb }}".upper() != "Y":
+        remove_mongodb()
 
     print(SUCCESS + "Your Open edX devops repo has been initialized." + TERMINATOR)
 
