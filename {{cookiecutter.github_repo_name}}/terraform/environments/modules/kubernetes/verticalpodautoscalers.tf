4bb477a49f16f0549388ff41b6f94f28993272af
lpm0073 <lpm0073@gmail.com>
2022-09-12 14:56:22 -0500

    add dependency to kubernetes_namespace.environment_namespace

--- {{cookiecutter.github_repo_name}}/terraform/environments/modules/kubernetes/verticalpodautoscalers.tf
+++ {{cookiecutter.github_repo_name}}/terraform/environments/modules/kubernetes/verticalpodautoscalers.tf
@@ -99,57 +99,113 @@ data "template_file" "vpa-smtp" {
 
 resource "kubectl_manifest" "vpa-lms" {
   yaml_body = data.template_file.vpa-lms.rendered
+
+  depends_on = [
+    kubernetes_namespace.environment_namespace
+  ]
 }
 
 resource "kubectl_manifest" "vpa-lms-worker" {
   yaml_body = data.template_file.vpa-lms-worker.rendered
+
+  depends_on = [
+    kubernetes_namespace.environment_namespace
+  ]
 }
 
 
 resource "kubectl_manifest" "vpa-cms" {
   yaml_body = data.template_file.vpa-cms.rendered
+
+  depends_on = [
+    kubernetes_namespace.environment_namespace
+  ]
 }
 
 resource "kubectl_manifest" "vpa-cms-worker" {
   yaml_body = data.template_file.vpa-cms-worker.rendered
+
+  depends_on = [
+    kubernetes_namespace.environment_namespace
+  ]
 }
 
 resource "kubectl_manifest" "vpa-mfe" {
   yaml_body = data.template_file.vpa-mfe.rendered
+
+  depends_on = [
+    kubernetes_namespace.environment_namespace
+  ]
 }
 
 resource "kubectl_manifest" "vpa-elasticsearch" {
   yaml_body = data.template_file.vpa-elasticsearch.rendered
+
+  depends_on = [
+    kubernetes_namespace.environment_namespace
+  ]
 }
 
 resource "kubectl_manifest" "vpa-discovery" {
   yaml_body = data.template_file.vpa-discovery.rendered
+
+  depends_on = [
+    kubernetes_namespace.environment_namespace
+  ]
 }
 
 resource "kubectl_manifest" "mongodb" {
   yaml_body = data.template_file.vpa-mongodb.rendered
+
+  depends_on = [
+    kubernetes_namespace.environment_namespace
+  ]
 }
 
 resource "kubectl_manifest" "notes" {
   yaml_body = data.template_file.vpa-notes.rendered
+
+  depends_on = [
+    kubernetes_namespace.environment_namespace
+  ]
 }
 
 resource "kubectl_manifest" "smtp" {
   yaml_body = data.template_file.vpa-smtp.rendered
+
+  depends_on = [
+    kubernetes_namespace.environment_namespace
+  ]
 }
 
 resource "kubectl_manifest" "nginx" {
   yaml_body = data.template_file.vpa-nginx.rendered
+
+  depends_on = [
+    kubernetes_namespace.environment_namespace
+  ]
 }
 
 resource "kubectl_manifest" "cert-manager" {
   yaml_body = data.template_file.vpa-cert-manager.rendered
+
+  depends_on = [
+    kubernetes_namespace.environment_namespace
+  ]
 }
 
 resource "kubectl_manifest" "cert-manager-cainjector" {
   yaml_body = data.template_file.vpa-cert-manager-cainjector.rendered
+
+  depends_on = [
+    kubernetes_namespace.environment_namespace
+  ]
 }
 
 resource "kubectl_manifest" "cert-manager-webhook" {
   yaml_body = data.template_file.vpa-cert-manager-webhook.rendered
+
+  depends_on = [
+    kubernetes_namespace.environment_namespace
+  ]
 }
