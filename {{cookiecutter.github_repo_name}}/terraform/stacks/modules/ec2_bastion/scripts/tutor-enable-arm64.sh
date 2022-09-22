b3f4358ad845da509dce2b204bc611bfd74b3f3a
lpm0073 <lpm0073@gmail.com>
2022-09-22 18:11:04 -0500

    fix typo

--- {{cookiecutter.github_repo_name}}/terraform/stacks/modules/ec2_bastion/scripts/tutor-enable-arm64.sh
+++ {{cookiecutter.github_repo_name}}/terraform/stacks/modules/ec2_bastion/scripts/tutor-enable-arm64.sh
@@ -8,6 +8,6 @@
 # usage:      enables arb64 helpers for tutor installs to M1 arm64-base architectures
 #--------------------------------------------------------
 
-git clone https://github.com/open-craft/tutor-contrib-TUTOR_ARM64.git
+git clone https://github.com/open-craft/tutor-contrib-arm64.git
 tutor plugins enable arm64
 tutor config save --set DOCKER_IMAGE_MYSQL=mariadb:10.4
