3e0b223b9ddf7d3ea8605a99b1005e581b91f0a7
lpm0073 <lpm0073@gmail.com>
2022-10-21 13:51:16 -0500

    add tutor dev for macOS

---
+++ {{cookiecutter.github_repo_name}}/scripts/tutor-macos.sh
@@ -0,0 +1,17 @@
+export TUTOR_ROOT="~/.tutor"
+[ ! -d "${TUTOR_ROOT}" ] && mkdir $TUTOR_ROOT
+
+tutor config save --interactive
+
+# resolves a macOS-specific compatibility problem with the
+# mysql5.7 container.
+tutor config save --set DOCKER_IMAGE_MYSQL=mariadb:10.4
+
+# Open Craft plugin that helps with arm64 computers
+pip install git+https://github.com/open-craft/tutor-contrib-arm64
+tutor plugins enable arm64
+
+tutor images build openedx permissions
+tutor dev dc build lms
+tutor dev start -d
+tutor dev init
