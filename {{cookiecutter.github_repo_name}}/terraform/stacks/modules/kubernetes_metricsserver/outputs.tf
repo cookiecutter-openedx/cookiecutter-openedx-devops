701473a64ea4e5db7a77d8c6c8a5c731f42ce539
lpm0073 <lpm0073@gmail.com>
2023-01-11 14:36:28 -0600

    add some outputs so that terragrunt won't complain

--- {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes_metricsserver/outputs.tf
+++ {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes_metricsserver/outputs.tf
@@ -6,3 +6,18 @@
 #
 # usage: create an EKS cluster
 #------------------------------------------------------------------------------
+
+output "id" {
+  description = "The ID of the metrics-server"
+  value       = helm_release.metrics_server.id
+}
+
+output "name" {
+  description = "The name of the metrics-server"
+  value       = helm_release.metrics_server.name
+}
+
+output "namespace" {
+  description = "The namespace of the metrics-server"
+  value       = helm_release.metrics_server.namespace
+}
