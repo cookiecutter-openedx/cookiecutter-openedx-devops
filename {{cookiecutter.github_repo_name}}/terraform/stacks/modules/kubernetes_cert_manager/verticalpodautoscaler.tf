d0a5a32c8cdd93b6fb0221e16edfd4b2e8ceb5c8
lpm0073 <lpm0073@gmail.com>
2023-01-11 07:36:27 -0600

    vpa manifest resources depend on helm_release.cert-manager

--- {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes_cert_manager/verticalpodautoscaler.tf
+++ {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes_cert_manager/verticalpodautoscaler.tf
@@ -22,12 +22,23 @@ data "template_file" "vpa-cert-manager" {
 
 resource "kubectl_manifest" "vpa-cert-manager" {
   yaml_body = data.template_file.vpa-cert-manager.rendered
+
+  depends_on = [
+    helm_release.cert-manager
+  ]
 }
 
 resource "kubectl_manifest" "vpa-cert-manager-cainjector" {
   yaml_body = data.template_file.vpa-cert-manager-cainjector.rendered
+
+  depends_on = [
+    helm_release.cert-manager
+  ]
 }
 
 resource "kubectl_manifest" "vpa-cert-manager-webhook" {
   yaml_body = data.template_file.vpa-cert-manager-webhook.rendered
+  depends_on = [
+    helm_release.cert-manager
+  ]
 }
