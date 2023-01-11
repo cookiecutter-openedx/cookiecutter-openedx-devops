2c2addbe58937efef593e7348970b51ed0beda3f
lpm0073 <lpm0073@gmail.com>
2023-01-11 14:41:41 -0600

    add some outputs so that terragrunt won't complain

--- {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes_prometheus/outputs.tf
+++ {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes_prometheus/outputs.tf
@@ -6,3 +6,18 @@
 #
 # usage: create an EKS cluster
 #------------------------------------------------------------------------------
+
+output "id" {
+  description = "The ID of the metrics-server"
+  value       = helm_release.prometheus.id
+}
+
+output "name" {
+  description = "The name of the metrics-server"
+  value       = helm_release.prometheus.name
+}
+
+output "namespace" {
+  description = "The namespace of the metrics-server"
+  value       = helm_release.prometheus.namespace
+}
