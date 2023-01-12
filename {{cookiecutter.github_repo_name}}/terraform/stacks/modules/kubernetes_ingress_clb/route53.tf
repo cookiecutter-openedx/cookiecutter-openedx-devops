ce8c78bd7f4f17c9cd3966b5805b5c8861905211
lpm0073 <lpm0073@gmail.com>
2023-01-12 13:51:08 -0600

    remove A records to root domain. we don't want these anymore

--- {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes_ingress_clb/route53.tf
+++ {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes_ingress_clb/route53.tf
@@ -32,30 +32,6 @@ data "aws_route53_zone" "root_domain" {
   name = var.root_domain
 }
 
-resource "aws_route53_record" "root_naked" {
-  zone_id = data.aws_route53_zone.root_domain.id
-  name    = var.root_domain
-  type    = "A"
-
-  alias {
-    name                   = data.kubernetes_service.ingress_nginx_controller.status.0.load_balancer.0.ingress.0.hostname
-    zone_id                = data.aws_elb_hosted_zone_id.main.id
-    evaluate_target_health = true
-  }
-}
-
-resource "aws_route53_record" "root_wildcard" {
-  zone_id = data.aws_route53_zone.root_domain.id
-  name    = "*.${var.root_domain}"
-  type    = "A"
-
-  alias {
-    name                   = data.kubernetes_service.ingress_nginx_controller.status.0.load_balancer.0.ingress.0.hostname
-    zone_id                = data.aws_elb_hosted_zone_id.main.id
-    evaluate_target_health = true
-  }
-}
-
 # -------------------------------------------------------------------------------------
 # setup DNS for admin subdomain
 # -------------------------------------------------------------------------------------
