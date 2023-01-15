cd0c65d23bdeb701aed4321503428e4b13b9c9c3
lpm0073 <lpm0073@gmail.com>
2023-01-15 14:05:22 -0600

    set host name to route53 subdomain

--- {{cookiecutter.github_repo_name}}/terraform/stacks/modules/mysql/kubernetes.tf
+++ {{cookiecutter.github_repo_name}}/terraform/stacks/modules/mysql/kubernetes.tf
@@ -30,7 +30,7 @@ resource "kubernetes_secret" "mysql_root" {
   data = {
     MYSQL_ROOT_USERNAME = module.db.db_instance_username
     MYSQL_ROOT_PASSWORD = module.db.db_instance_password
-    MYSQL_HOST          = module.db.db_instance_address
+    MYSQL_HOST          = local.host_name
     MYSQL_PORT          = module.db.db_instance_port
   }
 }
