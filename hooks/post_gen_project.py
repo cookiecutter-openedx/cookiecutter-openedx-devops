b47fef97b253359c3d95e6d98184ba5eaadcd886
lpm0073 <lpm0073@gmail.com>
2023-01-29 13:43:40 -0600

    tests

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -46,19 +46,20 @@ def remove_mongodb():
     dir_path = os.path.join("terraform", "stacks", "{{ cookiecutter.global_platform_shared_resource_identifier }}", "mongodb")
     rm_directory(dir_path)
 
+    dir_path = os.path.join("terraform", "environments", "modules", "mongodb")
+    rm_directory(dir_path)
+
+    dir_path = os.path.join("terraform", "environments", "{{ cookiecutter.environment_name }}", "mongodb")
+    rm_directory(dir_path)
+
+def remove_mongodb_volume():
     dir_path = os.path.join("terraform", "stacks", "modules", "mongodb_volume")
     rm_directory(dir_path)
 
     dir_path = os.path.join("terraform", "stacks", "{{ cookiecutter.global_platform_shared_resource_identifier }}", "mongodb_volume")
     rm_directory(dir_path)
 
-    dir_path = os.path.join("terraform", "environments", "modules", "mongodb")
-    rm_directory(dir_path)
-
     dir_path = os.path.join("terraform", "environments", "modules", "mongodb_volume")
-    rm_directory(dir_path)
-
-    dir_path = os.path.join("terraform", "environments", "{{ cookiecutter.environment_name }}", "mongodb")
     rm_directory(dir_path)
 
     dir_path = os.path.join("terraform", "environments", "{{ cookiecutter.environment_name }}", "mongodb_volume")
@@ -68,28 +69,28 @@ def remove_k8s_dashboard():
     dir_path = os.path.join("terraform", "stacks", "modules", "kubernetes_dashboard")
     rm_directory(dir_path)
 
-    dir_path = os.path.join("terraform", "environments", "modules", "kubernetes_dashboard")
+    dir_path = os.path.join("terraform", "stacks", "{{ cookiecutter.global_platform_shared_resource_identifier }}", "kubernetes_dashboard")
     rm_directory(dir_path)
 
 def remove_k8s_kubeapps():
     dir_path = os.path.join("terraform", "stacks", "modules", "kubernetes_kubeapps")
     rm_directory(dir_path)
 
-    dir_path = os.path.join("terraform", "environments", "modules", "kubernetes_kubeapps")
+    dir_path = os.path.join("terraform", "stacks", "{{ cookiecutter.global_platform_shared_resource_identifier }}", "kubernetes_kubeapps")
     rm_directory(dir_path)
 
 def remove_k8s_karpenter():
     dir_path = os.path.join("terraform", "stacks", "modules", "kubernetes_karpenter")
     rm_directory(dir_path)
 
-    dir_path = os.path.join("terraform", "environments", "modules", "kubernetes_karpenter")
+    dir_path = os.path.join("terraform", "stacks", "{{ cookiecutter.global_platform_shared_resource_identifier }}", "kubernetes_karpenter")
     rm_directory(dir_path)
 
 def remove_k8s_prometheus():
     dir_path = os.path.join("terraform", "stacks", "modules", "kubernetes_prometheus")
     rm_directory(dir_path)
 
-    dir_path = os.path.join("terraform", "environments", "modules", "kubernetes_prometheus")
+    dir_path = os.path.join("terraform", "stacks", "{{ cookiecutter.global_platform_shared_resource_identifier }}", "kubernetes_prometheus")
     rm_directory(dir_path)
 
 def remove_bastion():
@@ -101,56 +102,9 @@ def remove_bastion():
     )
     rm_directory(dir_path)
 
-    dir_path = os.path.join("terraform", "environments", "modules", "ec2_bastion")
-    rm_directory(dir_path)
-
-    dir_path = os.path.join(
-        "terraform",
-        "environments",
-        "{{ cookiecutter.environment_name }}",
-        "ec2_bastion",
-    )
-    rm_directory(dir_path)
-
-
-def remove_mongodb():
-    dir_path = os.path.join("terraform", "stacks", "modules", "mongodb")
-    rm_directory(dir_path)
-
-    dir_path = os.path.join(
-        "terraform",
-        "stacks",
-        "{{ cookiecutter.global_platform_shared_resource_identifier }}",
-        "mongodb",
-    )
-    rm_directory(dir_path)
-
-    dir_path = os.path.join("terraform", "stacks", "modules", "mongodb_volume")
-    rm_directory(dir_path)
-
-    dir_path = os.path.join(
-        "terraform",
-        "stacks",
-        "{{ cookiecutter.global_platform_shared_resource_identifier }}",
-        "mongodb_volume",
-    )
-    rm_directory(dir_path)
-
-    dir_path = os.path.join(
-        "terraform", "environments", "{{ cookiecutter.environment_name }}", "mongodb"
-    )
-    rm_directory(dir_path)
-
-    dir_path = os.path.join("terraform", "environments", "modules", "mongodb")
-    rm_directory(dir_path)
-
-
 def main():
     if "{{ cookiecutter.stack_add_bastion }}".upper() != "Y":
         remove_bastion()
-
-    if "{{ cookiecutter.stack_add_remote_mongodb }}".upper() != "Y":
-        remove_mongodb()
 
     if "{{ cookiecutter.stack_add_k8s_dashboard }}".upper() != "Y":
         remove_k8s_dashboard()
@@ -172,6 +126,7 @@ def main():
 
     if "{{ cookiecutter.stack_add_remote_mongodb }}".upper() != "Y":
         remove_mongodb()
+        remove_mongodb_volume
 
     print(SUCCESS + "Your Open edX devops repo has been initialized." + TERMINATOR)
 
