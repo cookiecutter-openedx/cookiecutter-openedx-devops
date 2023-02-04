4adf2e51830be7cb6d3136186da82ce395150166
lpm0073 <lpm0073@gmail.com>
2023-02-04 14:53:20 -0600

    un-comment kms config

--- {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes/main.tf
+++ {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes/main.tf
@@ -86,8 +86,8 @@ module "eks" {
   # aws kms get-key-policy --key-id ADD-YOUR-KEY-ID-HERE --region {{ cookiecutter.global_aws_region }} --policy-name default --output text
   create_kms_key = var.eks_create_kms_key
 
-  # un-comment this to add more IAM users to the KMS key owners list.
-  #kms_key_owners                  = ["arn:aws:iam::${var.account_id}:user/system/bastion-user/${var.namespace}-bastion"]
+  # add more IAM users to the KMS key owners list
+  kms_key_owners                  = ["arn:aws:iam::${var.account_id}:user/system/bastion-user/${var.namespace}-bastion"]
 
   tags = merge(
     var.tags,
