1e0091369324618d53faed708bca5473bed53a1a
lpm0073 <lpm0073@gmail.com>
2023-02-05 18:39:03 -0600

    convert yaml inputs into template files

--- {{cookiecutter.github_repo_name}}/terraform/environments/modules/wordpress/main.tf
+++ {{cookiecutter.github_repo_name}}/terraform/environments/modules/wordpress/main.tf
@@ -51,6 +51,30 @@ data "template_file" "serviceAccountAnnotations" {
   template = file("${path.module}/yml/service-account-annotations.yaml")
 }
 
+data "template_file" "wordpressPlugins" {
+  template = file("${path.module}/yml/wordpress-plugins.yaml")
+}
+
+data "template_file" "extraVolumes" {
+  template = file("${path.module}/yml/extra-volumes.yaml")
+}
+
+data "template_file" "extraVolumeMounts" {
+  template = file("${path.module}/yml/extra-volume-mounts.yaml")
+}
+
+data "template_file" "podLabels" {
+  template = file("${path.module}/yml/pod-labels.yaml")
+}
+
+data "template_file" "podAnnotations" {
+  template = file("${path.module}/yml/pod-annotations.yaml")
+}
+
+data "template_file" "nodeSelector" {
+  template = file("${path.module}/yml/node-selector.yaml")
+}
+
 data "template_file" "wordpress-values" {
   template = file("${path.module}/yml/wordpress-values.yaml.tpl")
   vars = {
@@ -62,18 +86,18 @@ data "template_file" "wordpress-values" {
     wordpressBlogName                 = "Cookiecutter Wordpress Site"
     wordpressExtraConfigContent       = ""
     wordpressConfigureCache           = false
-    wordpressPlugins                  = []
+    wordpressPlugins                  = data.template_file.wordpressPlugins.rendered
     apacheConfiguration               = ""
     smtpHost                          = ""
     smtpPort                          = ""
     smtpUser                          = ""
     smtpProtocol                      = ""
     allowEmptyPassword                = false
-    extraVolumes                      = []
-    extraVolumeMounts                 = []
-    podLabels                         = {}
-    podAnnotations                    = {}
-    nodeSelector                      = {}
+    extraVolumes                      = data.template_file.extraVolumes.rendered
+    extraVolumeMounts                 = data.template_file.extraVolumeMounts.rendered
+    podLabels                         = data.template_file.podLabels.rendered
+    podAnnotations                    = data.template_file.podAnnotations.rendered
+    nodeSelector                      = data.template_file.nodeSelector.rendered
     PersistenceExistingClaim          = ""
     PersistenceSelector               = data.template_file.PersistenceSelector.rendered
     serviceAccountCreate              = true
