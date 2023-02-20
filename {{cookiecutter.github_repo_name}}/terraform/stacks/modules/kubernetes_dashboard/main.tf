4cb7a4de470d1f57dc37f1cfce8381282c09f58f
lpm0073 <lpm0073@gmail.com>
2023-02-20 17:45:03 -0600

    refactor Kubernetes Dashboard

--- {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes_dashboard/main.tf
+++ {{cookiecutter.github_repo_name}}/terraform/stacks/modules/kubernetes_dashboard/main.tf
@@ -15,156 +15,31 @@
 #   The login page will ask for a token. To generate said token, run:
 #   kubectl -n kubernetes-dashboard create token kubernetes-dashboard
 #
+# helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+# helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard
+# helm search repo kubernetes-dashboard
+# helm show values kubernetes-dashboard/kubernetes-dashboard
+#
+# Get the Kubernetes Dashboard URL by running:
+#   export POD_NAME=$(kubectl get pods -n default -l "app.kubernetes.io/name=kubernetes-dashboard,app.kubernetes.io/instance=kubernetes-dashboard" -o jsonpath="{.items[0].metadata.name}")
+#   echo https://127.0.0.1:8443/
+#   kubectl -n default port-forward $POD_NAME 8443:8443
 #-----------------------------------------------------------
 
-data "template_file" "_01" {
-  template = file("${path.module}/yml/01_Namespace.yaml")
+data "template_file" "dashboard-values" {
+  template = file("${path.module}/yml/values.yaml")
 }
 
-data "template_file" "_02" {
-  template = file("${path.module}/yml/02_ServiceAccount.yaml")
-}
+resource "helm_release" "dashboard" {
+  name             = "common"
+  namespace        = "kubernetes-dashboard"
+  create_namespace = true
 
-data "template_file" "_03" {
-  template = file("${path.module}/yml/03_Service.yaml")
-}
+  chart      = "kubernetes-dashboard"
+  repository = "https://kubernetes.github.io/dashboard/"
+  version    = "~> 6.0"
 
-data "template_file" "_04" {
-  template = file("${path.module}/yml/04_Secret.yaml")
-}
-
-data "template_file" "_05" {
-  template = file("${path.module}/yml/05_Secret.yaml")
-}
-
-data "template_file" "_06" {
-  template = file("${path.module}/yml/06_Secret.yaml")
-}
-
-data "template_file" "_07" {
-  template = file("${path.module}/yml/07_ConfigMap.yaml")
-}
-
-data "template_file" "_08" {
-  template = file("${path.module}/yml/08_Role.yaml")
-}
-
-data "template_file" "_09" {
-  template = file("${path.module}/yml/09_ClusterRole.yaml")
-}
-
-data "template_file" "_10" {
-  template = file("${path.module}/yml/10_RoleBinding.yaml")
-}
-
-data "template_file" "_11" {
-  template = file("${path.module}/yml/11_ClusterRoleBinding.yaml")
-}
-
-data "template_file" "_12" {
-  template = file("${path.module}/yml/12_Deployment.yaml")
-}
-
-data "template_file" "_13" {
-  template = file("${path.module}/yml/13_Service.yaml")
-}
-
-data "template_file" "_14" {
-  template = file("${path.module}/yml/14_Deployment.yaml")
-}
-
-resource "kubectl_manifest" "_01" {
-  yaml_body = data.template_file._01.rendered
-}
-
-resource "kubectl_manifest" "_02" {
-  yaml_body = data.template_file._02.rendered
-
-  depends_on = [
-    kubectl_manifest._01
-  ]
-}
-resource "kubectl_manifest" "_03" {
-  yaml_body = data.template_file._03.rendered
-
-  depends_on = [
-    kubectl_manifest._02
-  ]
-}
-resource "kubectl_manifest" "_04" {
-  yaml_body = data.template_file._04.rendered
-
-  depends_on = [
-    kubectl_manifest._03
-  ]
-}
-resource "kubectl_manifest" "_05" {
-  yaml_body = data.template_file._05.rendered
-
-  depends_on = [
-    kubectl_manifest._04
-  ]
-}
-resource "kubectl_manifest" "_06" {
-  yaml_body = data.template_file._06.rendered
-
-  depends_on = [
-    kubectl_manifest._05
-  ]
-}
-resource "kubectl_manifest" "_07" {
-  yaml_body = data.template_file._07.rendered
-
-  depends_on = [
-    kubectl_manifest._06
-  ]
-}
-resource "kubectl_manifest" "_08" {
-  yaml_body = data.template_file._08.rendered
-
-  depends_on = [
-    kubectl_manifest._07
-  ]
-}
-resource "kubectl_manifest" "_09" {
-  yaml_body = data.template_file._09.rendered
-
-  depends_on = [
-    kubectl_manifest._08
-  ]
-}
-resource "kubectl_manifest" "_10" {
-  yaml_body = data.template_file._10.rendered
-
-  depends_on = [
-    kubectl_manifest._09
-  ]
-}
-resource "kubectl_manifest" "_11" {
-  yaml_body = data.template_file._11.rendered
-
-  depends_on = [
-    kubectl_manifest._10
-  ]
-}
-resource "kubectl_manifest" "_12" {
-  yaml_body = data.template_file._12.rendered
-
-  depends_on = [
-    kubectl_manifest._11
-  ]
-}
-resource "kubectl_manifest" "_13" {
-  yaml_body = data.template_file._13.rendered
-
-  depends_on = [
-    kubectl_manifest._12
-  ]
-}
-resource "kubectl_manifest" "_14" {
-  yaml_body = data.template_file._14.rendered
-
-  depends_on = [
-    kubectl_manifest._13
+  values = [
+    data.template_file.dashboard-values.rendered
   ]
 }
