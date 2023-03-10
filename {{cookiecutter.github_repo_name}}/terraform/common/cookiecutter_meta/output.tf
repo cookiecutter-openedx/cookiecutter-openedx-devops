a78e7c61fcaa6736b8507e1e95b6833c585c6cfc
lpm0073 <lpm0073@gmail.com>
2023-03-09 18:06:43 -0600

    force recreation of meta resource on each apply

--- {{cookiecutter.github_repo_name}}/terraform/common/cookiecutter_meta/output.tf
+++ {{cookiecutter.github_repo_name}}/terraform/common/cookiecutter_meta/output.tf
@@ -1,47 +1,7 @@
-output "cookiecutter_github_commit_date" {
-  value = replace(tostring(data.template_file.cookiecutter_github_commit_date.rendered), "\n", "")
-}
-
-output "cookiecutter_terraform_version" {
-  value = replace(tostring(data.template_file.cookiecutter_terraform_version.rendered), "\n", "")
-}
-
-output "cookiecutter_timestamp" {
-  value = replace(tostring(data.template_file.cookiecutter_timestamp.rendered), "\n", "")
-}
-
-output "cookiecutter_os" {
-  value = replace(tostring(data.template_file.cookiecutter_os.rendered), "\n", "")
-}
-
-output "cookiecutter_github_repository" {
-  value = replace(tostring(data.template_file.cookiecutter_github_repository.rendered), "\n", "")
-}
-
-output "cookiecutter_github_branch" {
-  value = replace(tostring(data.template_file.cookiecutter_github_branch.rendered), "\n", "")
-}
-
-output "cookiecutter_github_commit" {
-  value = replace(tostring(data.template_file.cookiecutter_github_commit.rendered), "\n", "")
-}
-
-output "cookiecutter_global_iam_arn" {
-  value = replace(tostring(data.template_file.cookiecutter_global_iam_arn.rendered), "\n", "")
-}
-
-output "cookiecutter_kubectl_version" {
-  value = replace(tostring(data.template_file.cookiecutter_kubectl_version.rendered), "\n", "")
-}
-
-output "cookiecutter_awscli_version" {
-  value = replace(tostring(data.template_file.cookiecutter_awscli_version.rendered), "\n", "")
-}
-
 output "tags" {
   value = {
     "cookiecutter/meta/terraform"                      = "true"
-    "cookiecutter/meta/version"                        = "v1.0.26"
+    "cookiecutter/meta/version"                        = replace(tostring(data.template_file.cookiecutter_version.rendered), "\n", "")
     "cookiecutter/meta/aws_iam_user"                   = replace(tostring(data.template_file.cookiecutter_global_iam_arn.rendered), "\n", "")
     "cookiecutter/meta/github_repository"              = replace(tostring(data.template_file.cookiecutter_github_repository.rendered), "\n", "")
     "cookiecutter/meta/github_branch"                  = replace(tostring(data.template_file.cookiecutter_github_branch.rendered), "\n", "")
